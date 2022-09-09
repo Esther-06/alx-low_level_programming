eb4be16e9e798bc2644c03b692a93b687cac2062
Esther-06 <wairimuw@gmail.com>
2022-09-08 18:37:33 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- *
  * main - Prints a text
  *
  * Description: using the main function
- * this program prints "programming is multilingual puzzle"
  * Return: Always 0 (Successful)
  *
  */
