1d0992b634f2645e56c267515b2775964689d53f
Esther-06 <wairimuw@gmail.com>
2022-09-14 09:19:26 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets in lowercase
+ * Returns: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int num = 1;
+
+	while (num <= 10)
+	{
+		alphabet = 'a';
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+		num++
+	}
+}
