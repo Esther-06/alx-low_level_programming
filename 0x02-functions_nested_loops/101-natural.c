1d60c735cf34bcf3a0510d325c398cecc38b1a81
Esther-06 <wairimuw@gmail.com>
2022-09-14 17:41:55 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,9 @@
 #include <math.h>
 /**
  * main - Entry point
- * Description: computes sum of all multiples of 3 or 5 below 1024 excluded
+ * Description: computes the sum of all
+ *		 multiples of 3 or 5
+ *		 below (1024) excluded
  * Return: Always 0 (Success)
  */
 
@@ -13,7 +15,7 @@ int main(void)
 	int total = 0;
 		while (i < 1024)
 	{
-		if (i < 1024)
+		if (i % 3 == 0)
 			total += i;
 		else if (i % 5 == 0)
 			total += i;
