3e10298c3ce572e846b84769e380420a43ad97dc
Esther-06 <wairimuw@gmail.com>
2022-09-14 23:39:38 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  * main - prints positive or negative integer
- * Returns: 0 success
+ * Return: 0 success
  */
 int main(void)
 {
 	int i;
+
 	i = 0;
 	positive_or_negative(i);
-	retun (0);
+
+	return (0);
 }
