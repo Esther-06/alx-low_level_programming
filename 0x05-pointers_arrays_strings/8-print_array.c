68e11db491537f72fe48caf34aeb7160bfdf3709
Esther-06 <wairimuw@gmail.com>
2022-09-19 16:19:12 -0700

    element of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - prints n element of an array
+ * @num: number of data in an array
+ * @array: array passed in
+ */
+
+void print_array(int *array, int num)
+{
+	int i;
+
+	num = num - 1;
+	i - 0;
+	{
+		if (i == num)
+		{
+			printf("%d", array[i]);
+		}
+		else
+		{
+			printf("%d, ", array[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
