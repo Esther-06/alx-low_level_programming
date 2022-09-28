54f1fa7d26238a7c68db4fab529e9d461bbb2f95
Esther-06 <wairimuw@gmail.com>
2022-09-28 16:39:23 -0700

    string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse other
+ * @s: the string to be print in reverse other
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
