865373cd2fbc48a9aaf0e8c9cd477e7b01dd8d41
Esther-06 <wairimuw@gmail.com>
2022-10-03 18:03:14 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,7 +34,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	for (x = 0; x < height; y++)
+	for (x = 0; x < height; x++)
 	{
 		for (y = 0; y < width; y++)
 			array[x][y] = 0;
